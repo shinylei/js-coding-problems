e18502362e6021f41e44b88574ec5a217abab885
shinylei <walei@umich.edu>
2018-09-13 15:40:58 +0800

    add

---
+++ commonParentNode.js
@@ -0,0 +1,14 @@
+/*
+	查找两个节点的最近的一个共同父节点，可以包括节点自身
+*/
+
+
+function commonParentNode(oNode1, oNode2) {
+    while (oNode1) {
+        if (oNode1.contains(oNode2)) {
+            return oNode1;
+        }
+        oNode1 = oNode1.parentNode;
+    }
+    
+}
